5094b695467dc14ac6c99ada3d6f44dcfbd7eaa2
Lana Hayes Redmond <lana.hayes@aspirant.com>
2021-06-23 15:04:57 -0400

    Have message appear if pwd is too short

--- src/screens/TextScreen.js
+++ src/screens/TextScreen.js
@@ -2,19 +2,34 @@ import React, { useState } from "react";
 import { View, Text, StyleSheet, TextInput } from "react-native";
 
 const TextScreen = () => {
-  const [name, setName] = useState("");
+  const [password, setPassword] = useState("");
   return (
     <View>
-      <Text>Enter Name: </Text>
+      <Text>Enter Password:</Text>
       <TextInput
         style={styles.input}
         autoCapitalize="none"
         autoCorrect={false}
-        value={name}
-        onChangeText={(newText) => setName(newText)}
+        value={password}
+        onChangeText={(newText) => setPassword(newText)}
       />
-      <Text>My name is {name} </Text>
+
+      {password.length < 4 ? (
+        <Text>Password must be longer than 5 characters</Text>
+      ) : null}
     </View>
+    // <View>
+    //   <Text>Enter Name: </Text>
+    //   <TextInput
+    //     style={styles.input}
+    //     autoCapitalize="none"
+    //     autoCorrect={false}
+    //     value={name}
+    //     onChangeText={(newText) => setName(newText)}
+    //   />
+    //   <Text>My name is {name} </Text>
+    //   {true ? <Text>Facts, son</Text> : null}
+    // </View>
   );
 };
 
