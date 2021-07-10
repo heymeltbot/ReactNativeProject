1bf249d0d5ed19cad1bb40bd1d066ad7a0fd0080
Lana Hayes Redmond <lana.hayes@aspirant.com>
2021-07-10 12:48:49 -0400

    Flex box practice

--- src/screens/BoxScreen.js
+++ src/screens/BoxScreen.js
@@ -4,20 +4,36 @@ import { Text, View, StyleSheet } from "react-native";
 const BoxScreen = () => {
   return (
     <View style={styles.viewStyle}>
-      <Text style={styles.textStyle}>Box Screen</Text>
+      <View style={styles.redBox} />
+      <View style={styles.greenBox} />
+      <View style={styles.purpleBox} />
     </View>
   );
 };
 
 const styles = StyleSheet.create({
   viewStyle: {
-    borderWidth: 1,
-    borderColor: "black",
-  },
-  textStyle: {
     borderWidth: 3,
-    borderColor: "red",
-    margin: 20,
+    height: 100,
+    borderColor: "black",
+    flexDirection: "row",
+    justifyContent: "space-between",
+  },
+  redBox: {
+    height: 50,
+    width: 50,
+    backgroundColor: "red",
+  },
+  purpleBox: {
+    height: 50,
+    width: 50,
+    backgroundColor: "purple",
+  },
+  greenBox: {
+    height: 50,
+    width: 50,
+    backgroundColor: "green",
+    alignSelf: "flex-end",
   },
 });
 
